f1693c35a04fae04a5bd97fbe8c6b2359c2b1c26
Alejandro Tabares <alex311es@gmail.com>
2012-08-01 08:17:11 +0200

    Initial commit.

---
+++ required_by_role.js
@@ -0,0 +1,21 @@
+(function($) {
+/**
+ * Custom behavior for the module mandatory pero role
+ * States api does not solve the problem.
+ */
+Drupal.behaviors.requiredByRole= {
+  attach: function (context, settings) {
+
+    jQuery(':input[name="instance[required]"]').change(function(){
+      if(jQuery(this).is(':checked')){
+        jQuery('#edit-instance-settings-required-by-role :checkbox').attr('disabled', 'disabled');
+      }
+      else {
+       jQuery('#edit-instance-settings-required-by-role :checkbox').removeAttr('disabled');
+      }
+    })
+
+  }
+};
+
+})(jQuery);
