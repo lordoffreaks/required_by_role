6767c721d77c09d959fd6528fff94d7347697fc8
lord_of_freaks <alex311es@yahoo.es>
2014-07-23 10:15:01 +0100

    Fixing #2112363 and #2112381

--- required_by_role.js
+++ required_by_role.js
@@ -6,14 +6,22 @@
 Drupal.behaviors.requiredByRole = {
   attach: function (context, settings) {
 
-    jQuery(':input[name="instance[required]"]').change(function(){
-      if(jQuery(this).is(':checked')){
+    jQuery(':input[name="instance[required]"]').change(function() {
+      if(jQuery(this).is(':checked')) {
         jQuery('#tableselect-required-by-role').find(':checkbox').attr('disabled', 'disabled');
       }
       else {
        jQuery('#tableselect-required-by-role').find(':checkbox').removeAttr('disabled');
       }
-    })
+    });
+
+    jQuery('#tableselect-required-by-role :checkbox').change(function() {
+      if(jQuery(this).is(':checked')) {
+        jQuery(':input[name="instance[required]"]').attr('checked', false);
+      }
+    });
+
+    jQuery(':input[name="instance[required]"]').change();
 
   }
 };
